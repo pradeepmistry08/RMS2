b87df9b085815125bb7e7bfae9f508f395ab02b4
pradeepmistry08 <pradeephmistry@gmail.com>
2026-07-25 14:05:52 +0530

    Update api.js

--- api.js
+++ api.js
@@ -4,7 +4,7 @@
    ============================================================
    SETUP: paste your deployed Apps Script Web App URL below.
    ============================================================ */
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw0XGZrzPeOrHFZAblh2zU7M9fi7R88CyQ6fjtualmuz0wIZ2kD7d7yjpR5hRymkQLG/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwOYg7-1wEk3NV9vGPnd_cwuumvL7qaQdxw2ZbIXN4A0rbwUerAs86iHLiUdJWXvbjk/exec';
 
 function apiIsConfigured(){
   return typeof APPS_SCRIPT_URL === 'string' && APPS_SCRIPT_URL.indexOf('https://script.google.com') === 0;
